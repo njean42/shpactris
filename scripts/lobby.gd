d808f51e026e8e9242aa0b65b9feac90754b62c1
njean42 <gunoi@njean.me>
2020-05-05 21:59:14 +0200

    Better detection of who does what + access to who plays pacman and the ship.

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 var peer
 var clients = []
+var pacman = null
+var ship = null
 
 
 func _ready():
@@ -16,13 +18,15 @@ func _on_network_peer_connected(peer_id):
 	
 	if clients.size() == 2 and is_network_master():
 		rpc("set_clients",clients)
+		set_clients(clients)
 		rpc('start_game')
 		# TODO: server.set_refuse_new_network_connections(true)
 
 
 puppet func set_clients(actual_clients):
-	prints(get_tree().get_network_unique_id(),'set_clients()',actual_clients)
 	clients = actual_clients
+	pacman = clients[0]
+	ship = clients[1]
 
 
 func _on_network_peer_disconnected(peer_id):
@@ -40,10 +44,10 @@ remotesync func start_game():
 
 # utility functions to determine who executes what code (who is master over which game elements)
 func i_am_the_game():
-	return get_tree().get_network_unique_id() in [0,1]
+	return get_tree().get_network_peer() == null or get_tree().get_network_unique_id() == 1
 	
 func i_am_pacman():
-	return get_tree().get_network_unique_id() == 0 or $'/root/world/pacman'.is_network_master()
+	return get_tree().get_network_peer() == null or $'/root/world/pacman'.is_network_master()
 
 func i_am_the_ship():
-	return get_tree().get_network_unique_id() == 0 or $'/root/world/ship'.is_network_master()
+	return get_tree().get_network_peer() == null or $'/root/world/ship'.is_network_master()
