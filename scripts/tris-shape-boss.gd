7e14bd981aa540462c1a5a009d309208690a6e95
njean42 <gunoi@njean.me>
2020-05-09 21:59:37 +0200

    Fix; bosses fire many bad (red) bullets.

--- scripts/tris-shape-boss.gd
+++ scripts/tris-shape-boss.gd
@@ -21,7 +21,7 @@ func is_friendable():
 func fire_enemy_bullets():
 	var nb_bullets = 8
 	for i in range(nb_bullets):
-		var type = 'regular' if randf() <= 0.9 else 'bad'
+		var type = 'bad' if randf() <= 0.9 else 'regular'
 		var dir = Vector2(0,1).rotated(2*PI/nb_bullets*i)
 		rpc("sync_fire_enemy_bullet",global_position,global.enemy_bullet_i,type,dir)
 		sync_fire_enemy_bullet(global_position,global.enemy_bullet_i,type,dir)
